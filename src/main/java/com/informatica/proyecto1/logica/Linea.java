328524ec7cfadef19801f0d3b39029b56f85069e
Victor Amaya <vamaya@asesoftware.com>
2020-03-27 23:41:24 -0500

    ajustes clase Linea

--- src/main/java/com/informatica/proyecto1/logica/Linea.java
+++ src/main/java/com/informatica/proyecto1/logica/Linea.java
@@ -17,17 +17,20 @@ import java.awt.Graphics2D;
  */
 public class Linea {
     //VARIABLES
-    private int xFinalEstatica, yFinalEstatica, xFinalDinamica, yFinalDinamica, xInicial, yInicial, ancho;//Coordenadas iniciales y finales del punto
-
+    private int xFinalDinamica, yFinalDinamica, xInicial, yInicial, ancho;//Coordenadas iniciales y finales del punto
+    private double h;
     //CONSTRUCTOR
     public Linea(int xFinal, int yFinal, int xInicial, int yInicial, int ancho) {
-        this.xFinalEstatica = xFinal;
-        this.yFinalEstatica = yFinal;
         this.xFinalDinamica = xFinal;
         this.yFinalDinamica = yFinal;
         this.xInicial = xInicial;
         this.yInicial = yInicial;
         this.ancho = ancho;
+        
+        //Se calcula la altura o el radio del vector(Hipotenusa)
+        double catetoOpuestoX = Math.abs(xInicial - xFinal);
+        double catetoAdyacenteY = Math.abs(yInicial - yFinal);
+        h = Math.hypot(catetoOpuestoX, catetoAdyacenteY);
     }
     
     //METODOS
@@ -45,12 +48,6 @@ public class Linea {
         //Movimiento en radianes
         double alpha = Math.toRadians(alphaGrados);
         
-        //TODO Mover h al constructor (clacular una sola vez) y anhadirle "final"
-        //Se calcula la altura o el radio del vector(Hipotenusa)
-        double catetoOpuestoX = Math.abs(xInicial - xFinalEstatica);
-        double catetoAdyacenteY = Math.abs(yInicial - yFinalEstatica);
-        double h = Math.hypot(catetoOpuestoX, catetoAdyacenteY);
-        
         //deltas de desplaxamiento de acuerdo al angulo
         double deltaX = h*Math.sin(alpha);
         double deltaY = h*Math.cos(alpha);
@@ -62,20 +59,20 @@ public class Linea {
         
         //Se revisa el valor de alpha y se asigna valor al corrimiento
         if( (alphaGrados>=0 && alphaGrados <=90)){
-            xm = xFinalEstatica + Math.abs(deltaX);
-            ym = (yFinalEstatica + h) - Math.abs(deltaY);
+            xm = xInicial + Math.abs(deltaX);
+            ym = yInicial - Math.abs(deltaY);
         }
         else if( (alphaGrados>90 && alphaGrados <180)){
-            xm = xFinalEstatica + Math.abs(deltaX);
-            ym = (yFinalEstatica + h) + Math.abs(deltaY);
+            xm = xInicial + Math.abs(deltaX);
+            ym = yInicial + Math.abs(deltaY);
         }
         else if( (alphaGrados>=180 && alphaGrados <=270) ){
-            xm = xFinalEstatica - Math.abs(deltaX);
-            ym = (yFinalEstatica + h) + Math.abs(deltaY);
+            xm = xInicial - Math.abs(deltaX);
+            ym = yInicial + Math.abs(deltaY);
         }
         else if( (alphaGrados>270 && alphaGrados <=360)){
-            xm = xFinalEstatica - Math.abs(deltaX);
-            ym = (yFinalEstatica + h) - Math.abs(deltaY);
+            xm = xInicial - Math.abs(deltaX);
+            ym = yInicial - Math.abs(deltaY);
         }
         
         //Se pasan los valores a las variables de la clase
@@ -95,10 +92,6 @@ public class Linea {
         //Se calcula el nuevo valor de las coordenadas en el extremo
         xFinalDinamica = xFinalDinamica - deltaBaseX;
         yFinalDinamica = yFinalDinamica - deltaBaseY;
-        
-        //Se cambia el valor de la base del extremo para hacer claculos
-        xFinalEstatica = xFinalEstatica - deltaBaseX;
-        yFinalEstatica = yFinalEstatica - deltaBaseY;
     }
     
     //GETTERS
